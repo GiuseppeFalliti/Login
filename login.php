fff92a60fc75a150118e86ec19aec3ef47d60e2a
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2024-11-08 15:12:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
 <?php
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
     $nomeUtente = $_POST["utente"];
     $nome = $_POST["nome"];
     $cognome = $_POST["cognome"];
@@ -12,7 +11,12 @@
     $dataNascita = $_POST["data"];
 
     printf("Dati: ". "Nome Utente:" .$nomeUtente ."|Nome:". $nome."|Cognome:". $cognome."|Password:". $password."|Data: ". $dataNascita);
-    
+    echo("<h1>Iscritto!</h1>");
+    echo("<h3>Nome Utente:</h3>".$nomeUtente);
+    echo("<h3>Nome:</h3>".$nome);
+    echo("<h3>Cognome:</h3>".$cognome);
+    echo("<h3>Password:</h3>".$password);
+    echo("<h3>Data Nascita:</h3>".$dataNascita);
 }
 ?>
 </body>
