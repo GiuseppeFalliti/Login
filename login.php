f3b7160dc7874c50232ea6fe6bfa2eae6e4b15ba
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2024-11-10 19:21:03 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,47 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Icrizione</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
+        * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins',sans-serif;
+}
+body{
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #dfdfdf;
+}
+.dati{
+    display: flex;
+    justify-content: center; 
+    flex-direction: column;
+    width: 440px;
+    height: 480px;
+    padding: 30px;
+}
+
+    </style>
+    </head>
 <body>
 <?php
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nomeUtente = $_POST["utente"];
-    $nome = $_POST["nome"];
-    $cognome = $_POST["cognome"];
+    $nomeUtente = $_POST["UserName"];
     $password = $_POST["password"];
-    $dataNascita = $_POST["data"];
-
-    printf("Dati: ". "Nome Utente:" .$nomeUtente ."|Nome:". $nome."|Cognome:". $cognome."|Password:". $password."|Data: ". $dataNascita);
-    echo("<h1>Iscritto!</h1>");
-    echo("<h3>Nome Utente:</h3>".$nomeUtente);
-    echo("<h3>Nome:</h3>".$nome);
-    echo("<h3>Cognome:</h3>".$cognome);
-    echo("<h3>Password:</h3>".$password);
-    echo("<h3>Data Nascita:</h3>".$dataNascita);
 }
 ?>
+<div class="dati">
+<h1>Hai Effetuato l'accesso</h1>
+<h3>Nome Utente: <?php echo($nomeUtente);?></h3>
+<h3>Password: <?php echo($password);?></h3>
 </body>
 </html>
+</div>
